f66dd12dd67c5ac87f0641d397c560e7bfdbac02
tyfighting <13271508702@126.com>
2019-10-18 18:06:57 +0800

    ges

--- src/components/ges/ges.vue
+++ src/components/ges/ges.vue
@@ -11,6 +11,7 @@ export default {
     return {};
   },
   mounted() {
+    this.$_split();
     setTimeout(() => (window.onresize = () => {
       console.log("mounted");
     }), 200);
@@ -18,7 +19,26 @@ export default {
   destroyed() {
     window.onresize=()=>console.log("destroyed");
   },
-  methods: {}
+  methods: {
+    $_split(){
+      let data = '12345678910.00120';
+      let [a, b] = data.split('.');
+      let reverseB=b.split('').reverse().join('');
+      reverseB=Number(reverseB).toString();
+      let B=reverseB.split('').reverse().join('');
+      let arr=[];
+      let length=Math.ceil(a.length/3)
+      for(let i=0;i<length;i++){
+        arr.push(a/1000>0?a%1000:a/1000)
+        a=parseInt(a/1000);
+      }
+      let A=arr.reverse().join(',');
+      let NewNumber=A+'.'+B;
+      console.log(data)
+      console.log(NewNumber)
+      return NewNumber;
+    }
+  }
 };
 </script>
 
